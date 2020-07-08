5833d62835a44659f863fa7bdb468e8345ef119d
dmitriyvolkov0 <67749034+dmitriyvolkov0@users.noreply.github.com>
2020-07-08 17:47:45 +0300

    Add files via upload

---
+++ BuhOne/js/menu.js
@@ -0,0 +1,38 @@
+let active=0;
+
+$('#menu_button').click(function(){
+    $(".submenu").animate({
+        height: "toggle"
+    }, 200, function () {
+            if (active == 0){
+                $('#menu_button').css({
+                    'transform': 'rotate(360deg)',
+                    'background':'url("../BuhOne/img/menu_close_but.png") center center no-repeat',
+                    'backgroundSize': 'contain',
+                    'transition': '0.3s'
+                });
+                active=1;
+            }else{
+                $('#menu_button').css({
+                    'transform': 'rotate(0deg)',
+                    'background': 'url("../BuhOne/img/menu_but.png") center center no-repeat',
+                    'backgroundSize': 'contain',
+                    'transition': '0.3s'
+                });
+                active=0;
+            }
+    });
+    
+    
+    
+});
+
+
+
+
+// $("#menu_button").animate({
+    
+// }, 200, function () {
+//     // Animation complete.
+// });
+
