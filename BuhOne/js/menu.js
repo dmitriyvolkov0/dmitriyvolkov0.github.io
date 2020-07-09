b8ac016f2c7da8ded676494c1b724047053169c6
dmitriyvolkov0 <67749034+dmitriyvolkov0@users.noreply.github.com>
2020-07-09 16:45:33 +0300

    Add files via upload

--- BuhOne/js/menu.js
+++ BuhOne/js/menu.js
@@ -7,7 +7,7 @@ $('#menu_button').click(function(){
             if (active == 0){
                 $('#menu_button').css({
                     'transform': 'rotate(360deg)',
-                    'background':'url("../BuhOne/img/menu_close_but.png") center center no-repeat',
+                    'background':'url("../BuhOne/img/menu_close_but.webp") center center no-repeat',
                     'backgroundSize': 'contain',
                     'transition': '0.3s'
                 });
@@ -15,7 +15,7 @@ $('#menu_button').click(function(){
             }else{
                 $('#menu_button').css({
                     'transform': 'rotate(0deg)',
-                    'background': 'url("../BuhOne/img/menu_but.png") center center no-repeat',
+                    'background': 'url("../BuhOne/img/menu_but.webp") center center no-repeat',
                     'backgroundSize': 'contain',
                     'transition': '0.3s'
                 });
